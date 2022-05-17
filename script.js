7d38be2c7af581a4737c59d78e8a8a60bab1d708
TIMuS09 <94984029+TIMuS09@users.noreply.github.com>
2022-05-17 15:49:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let menuMobile = document.querySelector('.header__menu-mobile');
+let burger = document.querySelector('.header__burger');
+let buttonBurger = document.querySelector('.header__burger-inner');
+// const burgerSpan = burger.querySelector('span');
+let headerInner = document.querySelector('.header__inner');
+let lock = document.querySelector('body');
+let menu = document.querySelector('.one');
+
+
+
+// buttonBurger.addEventListener('click',(event)=>{
+//     if(menuMobile.classList.contains('active')) {
+//         menuMobile.style.display = 'none';
+//         menuMobile.style.position = 'initial';
+//         menuMobile.style.left = '-100%'; 
+//         menuMobile.classList.remove('active');
+//         burgerSpan.style.transform = 'scale(1)';
+//     } else {
+//         menuMobile.style.display = 'block';
+//         menuMobile.style.position = 'fixed';
+//         menuMobile.style.left = '0'; 
+//         menuMobile.classList.add('active');
+//         burgerSpan.style.transform = 'scale(0)';
+//     }
+// })
+
+// buttonBurger.addEventListener('click', (event) => {
+//     if(headerInner.classList.contains('active')) {
+//         headerInner.classList.remove('active');
+//         lock.classList.remove('lock');
+//     } else {
+//         headerInner.classList.add('active');
+//         lock.classList.add('lock');
+//     }
+// })
+
+buttonBurger.addEventListener('click', (event) => {
+    headerInner.classList.toggle('active');
+    lock.classList.toggle('lock');
+})
+
+
+
+
+
+// крест на крест
+// сменить if на classList.toggle
+// scroll убрать
+// специфичность прочитать
+// создать вторую страницу
